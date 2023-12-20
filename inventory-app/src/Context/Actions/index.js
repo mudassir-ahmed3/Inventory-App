d53902acfc8ce75d5d4bf12cf7cf73ee0e3361f9
Mudassir Ahmed <36113857+mudassir-ahmed3@users.noreply.github.com>
2023-12-20 21:33:15 +0100

    Delete inventory-app directory

--- inventory-app/src/Context/Actions/index.js
+++
@@ -1,3 +0,0 @@
-// For Login Actions
-export const LOGIN_ACTION = 'LOGIN';
-export const LOGOUT_ACTION = 'LOGOUT';
--- inventory-app/src/Routes/Constant.js
+++
@@ -1,6 +0,0 @@
-const RoutesConstant = {
-    main:'/',
-    inventory: '/inventory'
-}
-
-export default RoutesConstant
